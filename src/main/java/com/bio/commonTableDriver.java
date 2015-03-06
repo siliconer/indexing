3bcaa59de021e7bd341e304479b68889b8e7989a
siliconexplorer1@gmail.com <siliconexplorer1@gmail.com>
2015-03-06 09:22:13 +0800

    commonTable

--- src/main/java/com/bio/commonTableDriver.java
+++ src/main/java/com/bio/commonTableDriver.java
@@ -1,5 +1,46 @@
 package com.bio;
 
-public class commonTableDriver {
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.Path;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.mapreduce.HFileOutputFormat;
+import org.apache.hadoop.hbase.mapreduce.PutSortReducer;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Job;
+import org.apache.hadoop.mapreduce.lib.input.FileInputFormat;
+import org.apache.hadoop.mapreduce.lib.output.FileOutputFormat;
 
+import org.apache.mahout.text.wikipedia.XmlInputFormat;
+
+public final class commonTableDriver {
+ public static void main(String[] args) throws Exception {
+  commonRunJob(args[0], args[1]);
+ }
+
+ public static void commonRunJob(String input,String output) throws Exception  {
+  Configuration conf = new Configuration();
+  conf.set("xmlinput.start", "<response>");
+  conf.set("xmlinput.end", "</response>");
+  
+  Job job = new Job(conf);
+  job.setJarByClass(commonTableMapper.class);
+  
+  job.setInputFormatClass(XmlInputFormat.class);
+  
+  job.setMapperClass(commonTableMapper.class);
+  job.setMapOutputKeyClass(Text.class);
+  job.setMapOutputValueClass(Text.class);
+  
+  job.setNumReduceTasks(0);
+  
+  FileInputFormat.setInputPaths(job, new Path(input));
+  Path outPath = new Path(output);
+  FileOutputFormat.setOutputPath(job, outPath);
+  
+  outPath.getFileSystem(conf).delete(outPath, true);
+  job.waitForCompletion(true);
+  
+ }
 }
--- src/main/java/com/bio/commonTableMapper.java
+++ src/main/java/com/bio/commonTableMapper.java
@@ -1,5 +1,98 @@
 package com.bio;
 
-public class commonTableMapper {
+import com.bio.xmlDef;
+import static javax.xml.stream.XMLStreamConstants.CHARACTERS;
+import static javax.xml.stream.XMLStreamConstants.START_ELEMENT;
+import javax.xml.stream.XMLInputFactory;
+import javax.xml.stream.XMLStreamReader;
 
+import java.io.ByteArrayInputStream;
+import java.io.IOException;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.hbase.HBaseConfiguration;
+import org.apache.hadoop.hbase.client.HTable;
+import org.apache.hadoop.hbase.client.Put;
+import org.apache.hadoop.hbase.io.ImmutableBytesWritable;
+import org.apache.hadoop.hbase.util.Bytes;
+import org.apache.hadoop.io.LongWritable;
+import org.apache.hadoop.io.Text;
+import org.apache.hadoop.mapreduce.Mapper;
+import org.apache.hadoop.mapreduce.Mapper.Context;
+import org.apache.hadoop.mapreduce.lib.input.FileSplit;
+
+public class commonTableMapper 
+ extends org.apache.hadoop.mapreduce.Mapper {
+  private HTable htable;
+  
+ // create HBase connection
+ protected void setup(Context context) 
+    throws IOException, InterruptedException {
+    Configuration conf = HBaseConfiguration.create();
+    htable = new HTable(conf, xmlDef.xmlDef[0][0]);
+    htable.setAutoFlush(true);
+    htable.setWriteBufferSize(1024 * 1024 * 12);
+    }
+   
+ public void map(LongWritable key, Text value, Mapper.Context context) 
+  throws IOException, InterruptedException {
+   String currLine = value.toString();
+   try {
+    XMLStreamReader reader = XMLInputFactory.newInstance()
+    .createXMLStreamReader(
+      new ByteArrayInputStream(currLine.getBytes()));
+ 
+    String currentElement = "";
+    int col = 0;
+    
+    // initialize all xml value to blank string
+    for (String xmlTag : xmlDef.xmlDef[3]) {
+     xmlDef.xmlDef[5][col] = "";
+     col++;
+    }
+    
+    
+    // read xml tags and store values in xmlDef
+    while (reader.hasNext()) {
+     int code = reader.next();
+     switch (code) {
+     case START_ELEMENT:
+      currentElement = reader.getLocalName();
+      break;
+     case CHARACTERS:
+      col = 0;
+      for (String xmlTag : xmlDef.xmlDef[3]) {
+       if (currentElement.equalsIgnoreCase(xmlTag)) {
+        xmlDef.xmlDef[5][col] += reader.getText().trim(); 
+       }
+       col++;
+      }
+     }
+    }
+    
+    
+    // writing values to mapper output file
+    // can remove this context.write
+    context.write(xmlDef.xmlDef[5][0]+"#"+xmlDef.xmlDef[5][1]+"#"+xmlDef.xmlDef[5][2]+"#"+xmlDef.xmlDef[5][3]+"#"+xmlDef.xmlDef[5][4],1);
+    
+    // put record in HBase
+    Put insHBase = new Put(Bytes.toBytes(xmlDef.xmlDef[5][0]));
+    col = 0;
+    for (String xmlTag : xmlDef.xmlDef[3]) {
+     insHBase.add(Bytes.toBytes(xmlDef.xmlDef[2][col]), Bytes.toBytes(xmlDef.xmlDef[3][col]), Bytes.toBytes(xmlDef.xmlDef[5][col]));
+     col++;
+    }
+    htable.put(insHBase);
+  
+  } catch (Exception e) {
+    e.printStackTrace();
+  }
+ }
+  @Override
+  public void cleanup(Context context) 
+   throws IOException, InterruptedException {
+   htable.flushCommits();
+   htable.close();
+   
+ }
 }
